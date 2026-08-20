75d5f68139c501f99f2aafa8763ae6d7fa133d55
valterzbaldi1 <valterzbaldi1@gmail.com>
2026-08-20 12:08:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import streamlit as st
 
 from finnhub_client import obter_preco
+from finnhub_client import obter_historico
 
-st.title("📈 Teste Finnhub")
+st.title("📈 Baldi Market Scanner")
 
-tickers = [
-    "NVDA",
-    "MSFT",
-    "META",
-    "AMZN"
-]
+ticker = "NVDA"
 
-for ticker in tickers:
+preco = obter_preco(ticker)
 
-    preco = obter_preco(ticker)
+historico = obter_historico(ticker)
 
-    st.write(f"{ticker} → ${preco}")
+st.subheader(ticker)
+
+st.write(f"Preço Atual: ${preco}")
+
+if historico is not None:
+
+    st.write(
+        f"Dias carregados: {len(historico)}"
+    )
+
+    st.line_chart(
+        historico["close"]
+    )
