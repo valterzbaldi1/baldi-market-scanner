3b1f01be5ad0bf142d778f7bc3980da77951e3cf
valterzbaldi1 <valterzbaldi1@gmail.com>
2026-08-20 13:07:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,13 +29,22 @@ for ticker in tickers:
 
     close = dados["Close"][ticker]
 
-    preco_atual = round(float(close.iloc[-1]), 2)
+    preco_atual = round(
+        float(close.iloc[-1]),
+        2
+    )
 
     rsi = calcular_rsi(close)
 
-    maxima = round(float(close.max()), 2)
+    maxima = round(
+        float(close.max()),
+        2
+    )
 
-    minima = round(float(close.min()), 2)
+    minima = round(
+        float(close.min()),
+        2
+    )
 
     distancia_maxima = round(
         ((preco_atual - maxima) / maxima) * 100,
@@ -51,68 +60,10 @@ for ticker in tickers:
         1
     )
 
-    col1, col2 = st.columns([0.5, 2.5])
+    col1, col2, col3 = st.columns([0.8, 2.3, 1.1])
 
     with col1:
 
         st.subheader(ticker)
 
-        st.write(f"💵 ${preco_atual}")
-
-        st.write(f"📈 RSI: {rsi}")
-
-        st.write(
-            f"📍 Posição: {posicao_historica}%"
-        )
-
-        st.write(
-            f"📉 Dist. Máx: {distancia_maxima}%"
-        )
-
-        if rsi < 40 and posicao_historica < 40:
-
-            st.success("🟢 COMPRAR")
-
-        elif rsi > 70 or posicao_historica > 80:
-
-            st.error("🔴 NÃO COMPRAR")
-
-        else:
-
-            st.warning("🟨 DÚVIDA")
-
-    with col2:
-
-        st.line_chart(close)
-
-        st.markdown("### 📰 Notícias")
-
-        noticias = obter_noticias(ticker)
-
-        if len(noticias) == 0:
-
-            st.info("NO NEWS")
-
-        else:
-
-            contador = 0
-
-            for noticia in noticias:
-
-                try:
-
-                    titulo = noticia["content"]["title"]
-
-                    if len(titulo) > 50:
-                        titulo = titulo[:50] + "..."
-
-                    st.write("• " + titulo)
-
-                    contador += 1
-
-                    if contador >= 3:
-                        break
-
-                except:
-
-                    pass
+      
