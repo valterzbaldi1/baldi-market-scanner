2a7a93bdd7085d2f1a8c6292df10e3d74b1e0a39
valterzbaldi1 <valterzbaldi1@gmail.com>
2026-08-20 12:28:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import yfinance as yf
 
 from indicators import calcular_rsi
+from news import obter_noticias
 
 st.title("📈 Baldi Market Scanner")
 
@@ -13,39 +14,54 @@ dados = yf.download(
     progress=False
 )
 
-# Ajuste para MultiIndex
 close = dados["Close"][ticker]
 
-preco_atual = round(
-    float(close.iloc[-1]),
-    2
-)
+preco_atual = round(float(close.iloc[-1]), 2)
 
 rsi = calcular_rsi(close)
 
-maxima = round(
-    float(close.max()),
-    2
-)
+maxima = round(float(close.max()), 2)
+
+minima = round(float(close.min()), 2)
+
+media = round(float(close.mean()), 2)
 
 distancia_maxima = round(
-    (
-        (preco_atual - maxima)
-        / maxima
-    ) * 100,
+    ((preco_atual - maxima) / maxima) * 100,
     2
 )
 
 st.subheader(ticker)
 
 st.write(f"💵 Preço Atual: ${preco_atual}")
-
 st.write(f"📈 RSI: {rsi}")
-
 st.write(f"🏔️ Máxima 6 meses: ${maxima}")
+st.write(f"📉 Mínima 6 meses: ${minima}")
+st.write(f"📊 Média 6 meses: ${media}")
+st.write(f"📉 Distância da Máxima: {distancia_maxima}%")
 
-st.write(
-    f"📉 Distância da Máxima: {distancia_maxima}%"
-)
+if rsi < 40 and distancia_maxima <= -10:
+    st.success("🟢 COMPRAR")
+
+elif rsi > 70:
+    st.error("🔴 NÃO COMPRAR")
+
+else:
+    st.warning("🟨 DÚVIDA")
+
+st.subheader("📰 Últimas Notícias")
+
+noticias = obter_noticias(ticker)
+
+for noticia in noticias:
+
+    try:
+        titulo = noticia["title"]
+        link = noticia["link"]
+
+        st.markdown(f"- {link}")
+
+    except:
+        pass
 
 st.line_chart(close)
