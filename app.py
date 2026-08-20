b1983ff7837b7833961601dbcd5325c594654a25
valterzbaldi1 <valterzbaldi1@gmail.com>
2026-08-20 11:06:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print("BALDI MARKET SCANNER")
+import streamlit as st
+
+st.set_page_config(
+    page_title="Baldi Market Scanner",
+    layout="wide"
+)
+
+st.title("📈 Baldi Market Scanner")
+
+st.subheader("Top Compras")
+
+st.write("1. SCHG")
+st.write("2. NVDA")
+st.write("3. META")
+
+st.subheader("Minha Carteira")
+
+st.write("MAIN → COMPRAR MAIS")
+st.write("JEPI → MANTER")
+st.write("O → MANTER")
