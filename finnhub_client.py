f874545712ab7b64f573ea2536ebc530022b8ada
valterzbaldi1 <valterzbaldi1@gmail.com>
2026-08-20 12:07:59 -0400

    Update finnhub_client.py

--- finnhub_client.py
+++ finnhub_client.py
@@ -1,5 +1,8 @@
 import requests
 import streamlit as st
+import pandas as pd
+from datetime import datetime, timedelta
+
 
 def obter_preco(ticker):
 
@@ -16,3 +19,34 @@ def obter_preco(ticker):
     dados = resposta.json()
 
     return dados["c"]
+
+
+def obter_historico(ticker):
+
+    api_key = st.secrets["FINNHUB_API_KEY"]
+
+    fim = int(datetime.now().timestamp())
+
+    inicio = int(
+        (datetime.now() - timedelta(days=180)).timestamp()
+    )
+
+    url = (
+        f"https://finnhub.io/api/v1/stock/candle"
+        f"?symbol={ticker}"
+        f"&resolution=D"
+        f"&from={inicio}"
+        f"&to={fim}"
+        f"&token={api_key}"
+    )
+
+    resposta = requests.get(url)
+
+    dados = resposta.json()
+
+    if dados["s"] != "ok":
+        return None
+
+    return pd.DataFrame({
+        "close": dados["c"]
+    })
