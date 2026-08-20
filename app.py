5059afbc66c4f134e0dfd7eb43d0410eaad8ecf4
valterzbaldi1 <valterzbaldi1@gmail.com>
2026-08-20 11:56:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,11 @@
 import streamlit as st
 
-from market_live import obter_dados
+from market_live import obter_preco
 
-st.set_page_config(
-    page_title="Baldi Market Scanner",
-    layout="wide"
-)
+st.title("📈 Teste Yahoo Finance")
 
-st.title("📈 Baldi Market Scanner")
+preco_nvda = obter_preco("NVDA")
 
-st.subheader("Teste de Dados Reais")
+st.write("NVDA")
 
-tickers = [
-    "NVDA",
-    "MSFT",
-    "SCHG",
-    "MAIN",
-    "JEPI"
-]
-
-for ticker in tickers:
-
-    dados = obter_dados(ticker)
-
-    st.markdown(
-        f"""
-### {dados['ticker']}
-
-💵 Preço Atual: ${dados['preco']}
-
-💰 Dividend Yield: {dados['yield']}
-"""
-    )
-
-    st.divider()
+st.write(preco_nvda)
