4ec9bb2e8a5ffd4b3f8d11fcfaa90c7723bc9b7c
valterzbaldi1 <valterzbaldi1@gmail.com>
2026-08-20 12:15:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,52 +1,16 @@
 import streamlit as st
 import yfinance as yf
 
-from indicators import calcular_rsi
-
-st.title("📈 Baldi Market Scanner")
-
-ticker = "NVDA"
+st.title("Diagnóstico Yahoo")
 
 dados = yf.download(
-    ticker,
+    "NVDA",
     period="6mo",
     progress=False
 )
 
-preco_atual = round(
-    float(dados["Close"].iloc[-1]),
-    2
-)
+st.write(type(dados))
 
-rsi = calcular_rsi(
-    dados["Close"]
-)
+st.write(dados.head())
 
-maxima = round(
-    float(dados["Close"].max()),
-    2
-)
-
-distancia_maxima = round(
-    (
-        (preco_atual - maxima)
-        / maxima
-    ) * 100,
-    2
-)
-
-st.subheader(ticker)
-
-st.write(f"💵 Preço Atual: ${preco_atual}")
-
-st.write(f"📈 RSI: {rsi}")
-
-st.write(f"🏔️ Máxima 6 meses: ${maxima}")
-
-st.write(
-    f"📉 Distância da Máxima: {distancia_maxima}%"
-)
-
-st.line_chart(
-    dados["Close"]
-)
+st.write(dados.columns)
